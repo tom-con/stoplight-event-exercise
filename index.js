c13a0ea0e3b214892f5ec551adf5adf1342bc2f8
Tom Conchie <tconchie@gmail.com>
2017-02-09 12:42:07 -0700

    Completes homework

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 (function() {
   'use strict';
+  var stopB = document.getElementById('stopButton');
+  var slowB = document.getElementById('slowButton');
+  var goB = document.getElementById('goButton');
+  var stop = document.getElementById('stopLight');
+  var slow = document.getElementById('slowLight');
+  var go = document.getElementById('goLight');
+  stopB.addEventListener("click", function(){
+    stop.classList.toggle("stop");
+  })
+  slowB.addEventListener("click", function(){
+    slow.classList.toggle("slow");
+  })
+  goB.addEventListener("click", function(){
+    go.classList.toggle("go");
+  })
 
-  // YOUR CODE HERE
 })();
